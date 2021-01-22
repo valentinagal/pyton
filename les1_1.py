b23ee9c5186e6fd439511524bfac624f1330f7b8
Valentina <valentinagalyam@gmail.com>
2021-01-22 23:08:42 +0200

    les1_1 was added

---
+++ les1_1.py
@@ -0,0 +1,7 @@
+num = input(" Enter your age: ")
+num = int(num)
+if num < 18:
+    print("Sorry, you are too young...")
+else:
+    name = input("Enter your name: ")
+    print(name + " welcome! What is your expectations?")
